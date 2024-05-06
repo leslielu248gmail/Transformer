764b2daabd3fe1be52e3fc515509f4477a067222
leslielu248gmail <164697310+leslielu248gmail@users.noreply.github.com>
2024-05-06 11:35:42 -0400

    Update transformer.py

--- transformer.py
+++ transformer.py
@@ -138,6 +138,47 @@ def DecoderBlock(nn.Module):
     return out
 
 
+class Decoder(nn.Module):
+  def __init__(self,
+               trg_vocab_size,
+               embed_size,
+               num_layers,
+               heads,
+               forward_expansion,
+               dropout,
+               device,
+               max_length):
+                 
+    super(Decoder, self).__init__()
+    self.device = device
+    self.word_embedding = nn.Embedding(trg_vocab_size, embed_size)
+    self.positional_encoding = PositionalEncoding(embed_size, max_length, device)
+
+    self.layers  = nn.ModuleList([
+      DecoderBlock(
+        embed_size,
+        heads,
+        forward_expansion,
+        dropout,
+        device
+      )
+      for _ in range(num_layers)
+    ])
+
+    self.fc_out = nn.Linear(embed_size, trg_vocab_size)
+    self.dropout = nn.Dropout(dropout)
+
+  def forward(self, x, enc_out, src_mask, trg_mask):
+    x = self.dropout(self.word_embedding(x))
+    x = self.positional_encoding(x)
+
+    for layer in self.layers:
+      x = layer(x, enc_out, enc_out, src_mask, trg_mask)
+
+    out = self.fc_out(x)
+    return out
+  
+
 class Transformer(nn.Module):
   def __init__(self,
                src_vocab_size,
